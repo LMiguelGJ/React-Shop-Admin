5392616dab157b67a4e69a03a3f2b07e12d3e1fc
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2023-10-05 15:52:37 -0400

    Guardado del token en una cookie para mantener la sesión

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -27,8 +27,11 @@ function useProvideAuth() {
 
     const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
     if (access_token) {
-      console.log(access_token.access_token);
+      const token = access_token.access_token;
       Cookies.set('token', access_token.access_token, { expires: 5 });
+      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: user } = await axios.get(endPoints.auth.profile);
+      setUser(user);
     }
   };
 
