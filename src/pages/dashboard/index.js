c6e45b9105b365146cdb09c9ac8d3c23a515c0b3
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2023-10-07 15:13:17 -0400

    mejoras en el dasboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import endPoints from '@services/api';
 import useFetch from '@hooks/useFetch';
-// import { Chart } from '@common/Chart';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
@@ -12,18 +12,24 @@ const PRODUCT_OFFSET = 1;
 export default function Dashboard() {
   const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
 
-  // const categoryNames = products?.map((product) => product.category);
-  // const categoryCount = categoryNames?.map((category) => category.name);
-  // const countOccurrences = (arr) => arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
-  const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+  const categoryNames = products?.map((product) => product.category);
+  const categoryCount = categoryNames?.map((category) => category.name);
+  const countOccurrences = (arr) => arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+  const occurrences = countOccurrences(categoryCount);
+  const labels = [];
+  const Count = [];
+  for (let label in occurrences) {
+    labels.push(label);
+    Count.push(occurrences[label]);
+  }
 
   const data = {
     labels: labels,
     datasets: [
       {
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        label: 'Categories',
+        data: Count,
+        backgroundColor: ['#ffbb11', '#c0c0c0', '#50AF95', 'f3ba2f', '#2a71d0'],
         borderColor: 'rgb(255, 99, 132)',
         borderWidth: 1,
       },
@@ -32,15 +38,14 @@ export default function Dashboard() {
 
   return (
     <>
-      <Bar data={data}></Bar>
-      {/* <Chart className="mb-8 mt-2">{data}</Chart> */}
-      {/*  */}
+      <Bar className="mb-8 mt-2" data={data}></Bar>
+
       <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
         <div className="flex flex-1 justify-between sm:hidden">
-          <a href="login" className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
+          <a href="dashboard" className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
             Previous
           </a>
-          <a href="login" className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
+          <a href="dashboard" className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
             Next
           </a>
         </div>
@@ -52,52 +57,52 @@ export default function Dashboard() {
           </div>
           <div>
             <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-              <a href="login" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              <a href="dashboard" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
                 <span className="sr-only">Previous</span>
-                {/* <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" /> */}
+                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
               </a>
               {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
               <a
-                href="login"
+                href="dashboard"
                 aria-current="page"
                 className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 1
               </a>
               <a
-                href="login"
+                href="dashboard"
                 className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
               >
                 2
               </a>
               <a
-                href="login"
+                href="dashboard"
                 className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
               >
                 3
               </a>
               <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span>
               <a
-                href="login"
+                href="dashboard"
                 className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
               >
                 8
               </a>
               <a
-                href="login"
+                href="dashboard"
                 className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
               >
                 9
               </a>
               <a
-                href="login"
+                href="dashboard"
                 className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
               >
                 10
               </a>
-              <a href="login" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              <a href="dashboard" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
                 <span className="sr-only">Next</span>
-                {/* <ChevronRightIcon className="h-5 w-5" aria-hidden="true" /> */}
+                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
               </a>
             </nav>
           </div>
